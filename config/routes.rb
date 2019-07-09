667e36cdd65b2f363fc1b1ee70e79bee142b8cf2
Timo Lohmann <lohmanntimo@gmail.com>
2019-07-09 17:30:14 +0200

    routes bugfix

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   resources :matches
-  get 'games/myMatches'
   devise_for :users
 
   root 'games#index'
   get 'games/about'
+  get 'games/myMatches'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

b9760fb465a7a0a4d32bb574f43b20fe4a2a6e20
Timo Lohmann <lohmanntimo@gmail.com>
2019-07-11 21:01:31 +0200

    heroku bugfix ... changed filter in controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -11,8 +11,9 @@ class GamesController < ApplicationController
   end
 
   def myMatches
-    @matches = Match.all.where("player1 = ? or player2 = ?", current_user.id, current_user.id).order('created_at DESC')
+    @matches = Match.all.order('created_at DESC')
     @matches_days = @matches.group_by {|match| match.created_at.to_date }
+
   end
 
 

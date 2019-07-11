c4fb23d501357b84715fa7860c095617b54da7fb
Timo Lohmann <lohmanntimo@gmail.com>
2019-07-11 20:49:55 +0200

    heroku bugfix.. deleted ordering matches

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
 def show
   @user = User.find(params[:id])
-  @my_matches = Match.all.where("player1 = ? or player2 = ?", @user.id,@user.id ).order('created_at DESC')
+  @my_matches = Match.all.where("player1 = ? or player2 = ?", @user.id,@user.id )
   #@my_matches_days = @matches.group_by {|match| match.created_at.to_date }
 end
 

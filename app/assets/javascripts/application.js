5c1b551c57ac7f88aa74071c44d41268a5b74334
Timo Lohmann <lohmanntimo@gmail.com>
2019-07-14 15:14:34 +0200

    layout changes; added serviceworker and manifest for pwa

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,11 @@
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/service-worker.js', { scope: './' })
+    .then(function(reg) {
+      console.log('[Companion]', 'Service worker registered!');
+      console.log(reg);
+    });
+}
+
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,11 @@ Rails.application.routes.draw do
   get 'games/ranking'
 
   match 'users/:id' => 'users#show', via: :get
+
+
+  # Service Worker Routes
+get '/service-worker.js' => "service_worker#service_worker"
+get '/manifest.json' => "service_worker#manifest"
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
